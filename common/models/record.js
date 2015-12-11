795f05e3c39e7e4c88c99f43f425c505bdae921f
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-12-11 10:38:51 +0000

    Disable some API methods for records and surveys that are not used
    Disable findOne, createChangeStream because are not used and exists
    as is duplicated.

--- common/models/record.js
+++ common/models/record.js
@@ -1,3 +1,5 @@
 module.exports = function(Record) {
-
+    Record.disableRemoteMethod('findOne', true);
+    Record.disableRemoteMethod('createChangeStream', true);
+    Record.disableRemoteMethod('exists', true);
 };
--- common/models/survey.js
+++ common/models/survey.js
@@ -1,3 +1,5 @@
 module.exports = function(Survey) {
-
+    Survey.disableRemoteMethod('findOne', true);
+    Survey.disableRemoteMethod('createChangeStream', true);
+    Survey.disableRemoteMethod('exists', true);
 };
